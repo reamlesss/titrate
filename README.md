e74506e66587f5ca3c0a53f432e2f58896bb9aac
Václav Vondráček <reamless@seznam.cz>
2025-01-29 21:26:03 +0100

    footer addition

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 <!-- README udela PDcko -->
+
+
+<!-- Zbytek jsou usefull odkazy -->
+
+https://github.com/kolking/react-native-rating
--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,12 @@ import Nav from 'react-bootstrap/Nav'
 import logo from './assets/logo.png'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 function App() {
   return (
     <>
-   <Navbar className="bg-red fixed-top w-100 text-light">
+      {/* NAVBAR */}
+      <Navbar className="bg-red fixed-top w-100 text-light">
         <Container fluid className="d-flex justify-content-start align-items-center">
-          
           <Navbar.Brand href="#home" className="text-light">
             <img
               alt="titrate logo"
@@ -20,8 +19,6 @@ function App() {
               className="align-top"
             />
           </Navbar.Brand>
-
-          
           <Nav className="justify-content-center">
             <Nav.Link href="#about" className="text-light">O nás</Nav.Link>
             <Nav.Link href="#services" className="text-light">Služby</Nav.Link>
@@ -30,8 +27,19 @@ function App() {
         </Container>
       </Navbar>
 
-      
+      {/* MAIN SECTION */}
       <div className="d-flex justify-content-center align-items-center vh-100 flex-column mt-5"></div>
+
+      {/* FOOTER */}
+      <footer className="bg-red text-center py-3 mt-auto">
+        <img
+          alt="titrate logo"
+          src={logo}
+          width="70"
+          height="60"
+          className="align-top"
+        />
+      </footer>
     </>
   )
 }
