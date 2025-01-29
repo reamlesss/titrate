adfd6b3caad62c62cf39aa54ce63340cefe3957e
Václav Vondráček <reamless@seznam.cz>
2025-01-29 10:51:09 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <img src="https://images.cooltext.com/5722336.gif" width="417" height="92" alt="TITRATING" />
+    </>
+  )
+}
+
+export default App
